71030344d27058eb0cd68cc8043a2266bb78e234
olenesheim <ole.s.nesheim@gmail.com>
2022-04-05 17:02:03 +0200

    test2

--- imu_read_and_write.py
+++ imu_read_and_write.py
@@ -3,15 +3,6 @@ from unicodedata import name
 from mpu6050 import mpu6050
 import time
 
-#Takes in filename and creates a .txt file for later use
-def create_file():
-    created_file = 0
-    return created_file
-
-#Takes in the specific sensor and returns the acceleration data (x, y and z)
-def get_acceleration(sensor):
-    return 0
-
 #Takes in a sensor and returns the total accelearation based on 3D pytagoras
 def get_total_acceleration(sensor):
     acc = sensor.get_accel_data()
@@ -21,25 +12,33 @@ def get_total_acceleration(sensor):
     total_acceleration = sqrt(acc_x*acc_x + acc_y*acc_y + acc_z*acc_z)
     return total_acceleration
 
+
+#Takes in filename and creates a .txt file for later use
+def create_file():
+    created_file = 0
+    return created_file
+
 #Takes in filename and writes data into the .txt file
 def write_to_file(filename, data):
     return 0
+
+
 
 def main():
     mpu1 = mpu6050(0x68, 1)     #Inititalizes mpu6050 on bus 1
     mpu2 = mpu6050(0x68, 3)     #Inititalizes mpu6050 on bus 3
 
-    while True:
+    f = open("test1.txt", "w+")
+
+#    while True:
+    for i in range(100):
 
         acc = get_total_acceleration(mpu2)
         print(acc)
+        f.write("%d. acc = %d\n" % i % acc)
 
-
-  #      acc1 = mpu1.get_accel_data()
- #       acc2 = mpu2.get_accel_data()
-
-#        print("Sensor 1: ", str(acc1['x']), "; Sensor 2: ", str(acc2['x']), "\n")
-        time.sleep(0.05)
+        time.sleep(0.5)
+    f.close()
 
 
 
