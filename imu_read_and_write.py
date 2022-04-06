8f498cf305c07f6ae82e0880ac92723a6a4c7b63
olenesheim <ole.s.nesheim@gmail.com>
2022-04-06 10:35:42 +0200

    added print

--- imu_read_and_write.py
+++ imu_read_and_write.py
@@ -14,35 +14,25 @@ def get_total_acceleration(sensor):
 
 #Takes in filename and writes data into the .txt file.
 #freq is the frequency of the measurements, no_datapoints is the number of datapoints
-def write_to_file(freq, no_datapoints, filename, sensor1, sensor2):
-    
+def write_to_file(freq, no_datapoints, filename, sensor1, sensor2):   
     f = open(filename, "w+")
 
     for i in range (no_datapoints):
-
         acc1 = get_total_acceleration(sensor1)
         acc2 = get_total_acceleration(sensor2)
-
+        print(f"{acc1:.5f}".split("+")[0], "       ", f"{acc2:.5f}".split("+")[0])
         acc1string = f"{acc1:.5f}".split("+")[0]
         acc2string = f"{acc2:.5f}".split("+")[0]
-
         f.write(f"{acc1string};{acc2string}\n")
-
         time.sleep(1/freq)
 
-    f.close()
-    
+    f.close()    
     return 0
-
-
 
 def main():
     mpu1 = mpu6050(0x68, 1)     #Inititalizes mpu6050 on bus 1
     mpu2 = mpu6050(0x68, 3)     #Inititalizes mpu6050 on bus 3
-
-    write_to_file(4, 100, "test5.txt", mpu1, mpu2)
-
-
+    write_to_file(4, 100, "test5.txt", mpu1, mpu2)  #Writes the sesnsor data from both sensors into a text file
 
 if __name__ == "__main__":
     main()
---
+++ read_and_plot_sensordata.py
@@ -0,0 +1,50 @@
+import readline
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Takes in filename and returns values separated by ; as arrays
+def file_to_array(filename):
+    lines = []
+    with open(filename) as f:
+        lines = f.readlines()
+
+    acc1 = []
+    acc2 = []
+
+    for i in lines:
+        acc1string = f"{i}".split(";")[0]
+        acc2string = f"{i}".split(";")[1]
+        acc1.append(float(acc1string))
+        acc2.append(float(acc2string))
+    
+    return acc1, acc2
+
+#Takes in 2 columns of acceleration and plots them and the difference between
+def plot_acceleration(acc1, acc2):
+    x = np.linspace(0, len(acc1) - 1, 100)
+    fig, axs = plt.subplots(2)
+    fig.suptitle("Sensors")
+    axs[0].plot(x, acc1)
+    axs[0].set_title("Sensor 1")
+    axs[1].plot(x, acc2)
+    axs[1].set_title("Sensor 2")
+    plt.show()
+
+    # plt.plot(x, acc1)
+    # plt.legend(["Sensor 1"])
+    # plt.title("Sensor 1")
+    # plt.plot(x, acc2)
+    # plt.legend(["Sensor 2"])
+    # plt.title("Sensor 2")
+    # plt.show()
+    
+    return 0
+
+
+def main():
+    acc1, acc2 = file_to_array("test8.txt")
+    plot_acceleration(acc1, acc2)
+    return 0
+
+if __name__=="__main__":
+    main()
