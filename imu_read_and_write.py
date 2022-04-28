d501ecc1ec91e6c39c17ec884cfca28b47c23398
olenesheim <ole.s.nesheim@gmail.com>
2022-04-28 17:35:35 +0200

    changed code

--- imu_read_and_write.py
+++ imu_read_and_write.py
@@ -12,22 +12,37 @@ def get_total_acceleration(sensor):
     total_acceleration = sqrt(acc_x*acc_x + acc_y*acc_y + acc_z*acc_z)
     return total_acceleration
 
+def get_raw_accel_data(sensor):
+    acc = sensor.get_accel_data()
+    acc_x = acc['x']
+    acc_y = acc['y']
+    acc_z = acc['z']
+    return acc_x, acc_y, acc_z
+
 #Takes in filename and writes data into the .txt file.
 #freq is the frequency of the measurements, no_datapoints is the number of datapoints
 def write_to_file(freq, no_datapoints, filename, sensor1, sensor2):   
     f = open(filename, "w+")
 
     for i in range (no_datapoints):
-        acc1 = get_total_acceleration(sensor1)  #Getting acceleration data from sensor 1 (still sensor)
-        acc2 = get_total_acceleration(sensor2)  #Getting acceleration data from sensor 2 (Chest)
+        acc1 = get_raw_accel_data(sensor1)
+        acc2 = get_raw_accel_data(sensor2)
+        print(acc1, "       ", acc2)
         #print(f"{acc1:.5f}".split("+")[0], "       ", f"{acc2:.5f}".split("+")[0])
-        full_acc1 = sensor1.get_accel_data()
-        full_acc2 = sensor2.get_accel_data()
-        print(full_acc1, "         ", full_acc2)
-        acc1string = f"{acc1:.5f}".split("+")[0]
-        acc2string = f"{acc2:.5f}".split("+")[0]
-        f.write(f"{acc1string};{acc2string}\n")
         time.sleep(1/freq)
+
+
+    # for i in range (no_datapoints):
+    #     acc1 = get_total_acceleration(sensor1)  #Getting acceleration data from sensor 1 (still sensor)
+    #     acc2 = get_total_acceleration(sensor2)  #Getting acceleration data from sensor 2 (Chest)
+    #     #print(f"{acc1:.5f}".split("+")[0], "       ", f"{acc2:.5f}".split("+")[0])
+    #     full_acc1 = sensor1.get_accel_data()
+    #     full_acc2 = sensor2.get_accel_data()
+    #     print(full_acc1, "         ", full_acc2)
+    #     acc1string = f"{acc1:.5f}".split("+")[0]
+    #     acc2string = f"{acc2:.5f}".split("+")[0]
+    #     f.write(f"{acc1string};{acc2string}\n")
+    #     time.sleep(1/freq)
 
     f.close()    
     return 0
